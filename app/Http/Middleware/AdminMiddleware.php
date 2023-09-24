9d91569077f00fbcb3e99b5eda46052631c9cac7
malith94batugedara <malithbatugedara@gmail.com>
2023-09-24 17:30:21 +0530

    middlewareadded

---
+++ app/Http/Middleware/AdminMiddleware.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AdminMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        if(Auth::check()){
+           if(Auth::user()->role == 1){
+             return $next($request);
+           }
+           else{
+             return redirect(route('home'))->with('status','You are not an admin!');
+           }
+        }
+        else{
+            return redirect(route('login'))->with('status','Please Log In!');
+        }
+    }
+}
